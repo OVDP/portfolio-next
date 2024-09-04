0b55d10eafa9b29a9f4b1c2ac818a00c20fb21c0
ovdp <othniel@pol-it.nl>
2024-09-04 22:55:15 +0200

    converted project to tsx and added to the page + made more components from index.html

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,24 @@
-
 import Navbar from "./components/navbar";
 import About from "./components/about";
+import Projects from "./components/project";
 export default function Home() {
   return (
     <>
-      <div style={{ background: 'url("assets/img/openart-make-an-minimalistic-background-of-a-red-horizon-using-the-colors-red-yellow-and-orange_k32-AOTo_upscaled.png") center / contain', '--bs-body-color': '#212529', '--bs-dark': 'var(--bs-body-bg)', '--bs-dark-rgb': '255,255,255', '--bs-light': '#212529', '--bs-light-rgb': '33,37,41' }}>
+      <div
+        style={{
+          background:
+            'url("assets/img/openart-make-an-minimalistic-background-of-a-red-horizon-using-the-colors-red-yellow-and-orange_k32-AOTo_upscaled.png") center / contain',
+          "--bs-body-color": "#212529",
+          "--bs-dark": "var(--bs-body-bg)",
+          "--bs-dark-rgb": "255,255,255",
+          "--bs-light": "#212529",
+          "--bs-light-rgb": "33,37,41",
+        }}
+      >
         <Navbar />
         <About />
+        <Projects />
       </div>
     </>
-  )
+  );
 }
