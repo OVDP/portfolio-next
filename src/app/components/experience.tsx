769e2161571618a46449a02ec3058fd1f1bb50ef
Othniel van de Pol <othnielvandepol@gmail.com>
2025-04-16 12:02:04 +0200

    babagi

--- src/app/components/experience.tsx
+++ src/app/components/experience.tsx
@@ -57,34 +57,33 @@ export default function experience() {
           </div>
         </div>
         <h1 style={{ fontSize: "18px", margin: "0px 10px 0px 10px" }}>
-          Internship at Holding the Drones
+          Internship at Kop Digitaal
         </h1>
-        <sub style={{ margin: "0px 10px 0px 10px" }}>08/2023 - 02/2024</sub>
+        <sub style={{ margin: "0px 10px 0px 10px" }}>02/2025 - now</sub>
         <div className="d-lg-flex d-xxl-flex justify-content-lg-center align-items-lg-center align-items-xxl-start">
           <div>
             <p style={{ margin: "0px 10px 0px 10px" }}>
-              During my internship at Holding the Drones, I focused on coding
-              multiple full-stack websites and applications, supporting their
-              software environment for drones.
+              During my studies at KW1C, 
+              I completed a graduation internship where I focused on developing back-end-heavy websites and applications, 
+              working on projects commissioned by Kop digitaal's clients.
             </p>
             <ul>
               <li>
-                Further programmed with: PHP, HTML, CSS, JavaScript, MySQL,
-                Node.js &amp; ReactJS.
+                Further techniques used: Laravel, HTML, Tailwind CSS, JavaScript,
+                Node.js, ReactJS, PHP, Wordpress &amp; Woocommerce.
               </li>
-              <li>Design with: Figma.</li>
-              <li>Planned with: Trello &amp; Excel.</li>
+              <li>Design with: Adobe xd.</li>
+              <li>Planned with: Jira</li>
               <li>
-                Hosted websites and databases using Linux, Docker, Apache,
-                FileZilla, Nginx &amp; Putty.
+                Hosted websites and databases using Herd, Wordpress, Tabelplus &amp; DBngin.
               </li>
             </ul>
           </div>
-          <div className="d-flex d-md-flex d-lg-flex justify-content-center justify-content-md-center justify-content-lg-center">
+          <div className="d-flex d-md-flex justify-content-center justify-content-md-center">
             <img
               className="float-lg-end"
-              src="assets/img/Logohtd.jpg"
-              style={{ width: "250px", position: "relative" }}
+              src="assets/img/kop.png"
+              style={{ width: "250px", position: "relative", backgroundColor: "white", padding: "20px", borderRadius: "10px" }}
             />
           </div>
         </div>
@@ -121,33 +120,34 @@ export default function experience() {
           </div>
         </div>
         <h1 style={{ fontSize: "18px", margin: "0px 10px 0px 10px" }}>
-          Internship at Kop Digitaal
+          Internship at Holding the Drones
         </h1>
-        <sub style={{ margin: "0px 10px 0px 10px" }}>02/2025 - now</sub>
+        <sub style={{ margin: "0px 10px 0px 10px" }}>08/2023 - 02/2024</sub>
         <div className="d-lg-flex d-xxl-flex justify-content-lg-center align-items-lg-center align-items-xxl-start">
           <div>
             <p style={{ margin: "0px 10px 0px 10px" }}>
-              During my studies at KW1C, 
-              I completed a graduation internship where I focused on developing back-end-heavy websites and applications, 
-              working on projects commissioned by Kop digitaal's clients.
+              During my internship at Holding the Drones, I focused on coding
+              multiple full-stack websites and applications, supporting their
+              software environment for drones.
             </p>
             <ul>
               <li>
-                Further techniques used: Laravel, HTML, Tailwind CSS, JavaScript,
-                Node.js, ReactJS, PHP, Wordpress &amp; Woocommerce.
+                Further programmed with: PHP, HTML, CSS, JavaScript, MySQL,
+                Node.js &amp; ReactJS.
               </li>
-              <li>Design with: Adobe xd.</li>
-              <li>Planned with: Jira</li>
+              <li>Design with: Figma.</li>
+              <li>Planned with: Trello &amp; Excel.</li>
               <li>
-                Hosted websites and databases using Herd, Wordpress, Tabelplus &amp; DBngin.
+                Hosted websites and databases using Linux, Docker, Apache,
+                FileZilla, Nginx &amp; Putty.
               </li>
             </ul>
           </div>
-          <div className="d-flex d-md-flex justify-content-center justify-content-md-center">
+          <div className="d-flex d-md-flex d-lg-flex justify-content-center justify-content-md-center justify-content-lg-center">
             <img
               className="float-lg-end"
-              src="assets/img/kop.png"
-              style={{ width: "250px", position: "relative", backgroundColor: "white", padding: "20px", borderRadius: "10px" }}
+              src="assets/img/Logohtd.jpg"
+              style={{ width: "250px", position: "relative" }}
             />
           </div>
         </div>
